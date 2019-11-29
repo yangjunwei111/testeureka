7bbf6578b42ecf91f8d8f9bc8e14eb2a39911c29
yangjunwei <409334301@qq.com>
2019-11-29 18:16:19 +0800

    cloud

--- eureka-client02/src/main/java/com/yangjunwei/EurekaClientApplication.java
+++ eureka-client02/src/main/java/com/yangjunwei/EurekaClientApplication.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-/**
- * 作者: LCG
- * 日期: 2019/11/29 11:33
- * 描述:
- */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
--- eureka-client02/src/main/java/com/yangjunwei/controller/TestClient02Controller.java
+++ eureka-client02/src/main/java/com/yangjunwei/controller/TestClient02Controller.java
@@ -1,16 +1,10 @@
 package com.yangjunwei.controller;
 
-import com.client02.api.Test01Api;
 import com.yangjunwei.api.Test01Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 作者: LCG
- * 日期: 2019/11/29 11:35
- * 描述:
- */
 
 @RestController
 public class TestClient02Controller {
