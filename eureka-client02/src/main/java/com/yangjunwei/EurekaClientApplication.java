25dbc31b59e5e2425d59ebbcd4917e895b4cf0dd
yangjunwei <409334301@qq.com>
2019-11-29 15:54:31 +0800

    yjw666

---
+++ eureka-client02/src/main/java/com/yangjunwei/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package com.yangjunwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 作者: LCG
+ * 日期: 2019/11/29 11:33
+ * 描述:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class);
+    }
+
+}
